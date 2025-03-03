7f850ae323be07c597efeb357e57557035fbeb43
Anfisa <anfis.igorevna@gmail.com>
2025-03-03 20:08:21 +0300

    Отбор астронавтов

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -117,6 +117,111 @@ def promotion_image():
 
 </body>
 </html>'''.format(url_style, url_image, *lines)
+
+
+@app.route('/form_sample', methods=['POST', 'GET'])
+def form_sample():
+    prof = sorted(i.capitalize() for i in
+                  ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач', 'инженер по терраформированию',
+                   'климатолог', 'специалист по радиационной защите'])
+    if request.method == 'GET':
+        return '''
+        <!doctype html>
+        <html lang="en">
+            <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+            crossorigin="anonymous">
+            <link rel="stylesheet" type="text/css" href="static/css/style.css"/>
+            <title>Пример формы</title>
+            </head>
+            <body>
+            <h1>Анекта претендента</h1>
+            <h2>на участие в миссии</h2>
+            <br>
+            <div>
+                <form class="login_form" method="post" enctype="multipart/form-data">
+                    <input type="text" class="form-control" id="text" placeholder="Введите фамилию" name="second_name">
+                    <br>
+                    <input type="text" class="form-control" id="text" placeholder="Введите имя" name="first_name">
+                    <br>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                    <br>
+                    <div class="form-group">
+                        <label for="classSelect">Какое у вас образование?</label>
+                        <select class="form-control" id="classSelect" name="class">
+                            <option>Общее образование</option>
+                            <option>Профессиональное образование</option>
+                            <option>Дополнительное образование</option>
+                            <option>Профессиональное обучение</option>
+                        </select>
+                        </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="profession">Какие у вас профессии?</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof1" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof2" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof3" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof4" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof5" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof6" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof7" id="prof">
+                        <label class="form-check-label" for="prof">{}</label><br>
+                        <input class="form-check-input" type="checkbox" name="prof8" id="prof">
+                        <label class="form-check-label" for="prof">{}</label>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="form-check">Укажите пол</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                            <label class="form-check-label" for="male">
+                            Мужской
+                            </label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                            <label class="form-check-label" for="female">
+                            Женский
+                            </label>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="about">Почему вы хотите принять участие в миссии?</label>
+                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                    </div>
+                    <br>
+                    <div class="form-group">
+                        <label for="photo">Приложите фотографию</label>
+                        <br>
+                        <input type="file" class="form-control-file" id="photo" name="file">
+                    </div>
+                    <div class="form-group form-check"><br>
+                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                    </div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Отправить</button>
+                </form>
+            </div>
+            </body>
+        </html>'''.format(*prof)
+    elif request.method == 'POST':
+        get_items = ['second_name', 'first_name', 'email', *[f'prof{i + 1}' for i in range(len(prof))], 'sex',
+                     'about', 'file', 'accept']
+        for item in get_items:
+            print(request.form.get(item))
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
