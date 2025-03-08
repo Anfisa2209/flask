b0b1e1c7768600a084d30e48f58731a2a93073ba
Anfisa <anfis.igorevna@gmail.com>
2025-03-08 14:54:54 +0300

    Тренировки в полёте/Список профессий

--- main.py
+++ main.py
@@ -9,5 +9,16 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = ['инженер', 'врач', 'строитель', 'пилот', 'штурман']
+    return render_template('list_prof.html', list=lst, professions=professions)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
