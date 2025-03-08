c19ab65e23871dce04dd25a664cf46d6ccd4de41
Anfisa <anfis.igorevna@gmail.com>
2025-03-08 15:45:14 +0300

    Двойная защита

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    astronaut_id = StringField('ID астронавта', validators=[DataRequired()])
+    astronaut_password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    cap_id = StringField('ID капитана', validators=[DataRequired()])
+    cap_password = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/<title>')
@@ -9,18 +22,17 @@ def index(title):
     return render_template('base.html', title=title)
 
 
-@app.route('/answer')
-@app.route('/auto_answer')
-def auto_answer():
-    data = {'title': 'Анкета',
-            'surname': "Watny",
-            'name': "Mark",
-            'education': "выше среднего",
-            'profession': "штурман марсхода",
-            'sex': "male",
-            'motivation': "всегда мечтал застрять на Марсе!",
-            'ready': 'True'}
-    return render_template('auto_answer.html', **data)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Успех')
 
 
 if __name__ == '__main__':
