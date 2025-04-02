e7d36c4441cc7ebb782130c04b667c7f386d685c
Anfisa <anfis.igorevna@gmail.com>
2025-04-02 11:55:49 +0300

    Двойная защита 2

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from SQL.data.department import Department
 from SQL.data.department_form import DepartmentForm
 from SQL.data.job_form import JobForm
 from SQL.data.jobs import Jobs
-from SQL.data.login_form import LoginForm
+from SQL.data.old_login_form import LoginForm as OldLoginForm
 from SQL.data.register_form import RegisterForm
 from SQL.data.users import User
 from api.jobs_api import jobs_bp
@@ -38,26 +38,39 @@ def distribution():
     return render_template('distribution.html', user_list=user_list)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = OldLoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html', title='Успех', content='Добро пожаловать на корабль')
+
+
 @login_manager.user_loader
 def load_user(user_id):
     db_ses = db_session.create_session()
     return db_ses.get(User, user_id)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        user = db_sess.query(User).filter(User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
-            login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
-    return render_template('login.html', title='Авторизация', form=form)
-
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         db_sess = db_session.create_session()
+#         user = db_sess.query(User).filter(User.email == form.email.data).first()
+#         if user and user.check_password(form.password.data):
+#             login_user(user, remember=form.remember_me.data)
+#             return redirect("/")
+#         return render_template('login.html',
+#                                message="Неправильный логин или пароль",
+#                                form=form)
+#     return render_template('login.html', title='Авторизация', form=form)
+#
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
